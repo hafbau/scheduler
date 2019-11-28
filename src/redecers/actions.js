fe6e14e8469f660be03b194f408b30575500b39f
Zhabskyi <poltavagaz@gmail.com>
2019-11-28 13:04:38 -0700

    Deleted index && action files from reducers

--- src/redecers/actions.js
+++
@@ -1,7 +0,0 @@
-import { useReducer } from 'react';
-import axios from "../axios-instance";
-import reducer from './reducer';
-import { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "./actionTypes";
-
-
-};
