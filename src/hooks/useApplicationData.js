d4bb09745ed9ae42f5a374fb92866ff3903185cc
Zhabskyi <poltavagaz@gmail.com>
2019-11-28 08:59:52 -0700

    Boiler plate for useApplicationData hook

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+const useApplicationData = initial => {
+
+
+  const state = () => {
+
+  };
+
+  const setDay = () => {
+
+  };
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  };
+};
+
+export default useApplicationData;
