3747fff87eb1f20c68b4fec76ae0b578aa6d5ed3
Zhabskyi <poltavagaz@gmail.com>
2019-11-28 10:12:39 -0700

    Make file structure for useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "../axios-instance";
 
 const useApplicationData = initial => {
