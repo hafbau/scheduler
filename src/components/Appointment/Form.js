c243ec6dea5cf54b2b8660a6b44f9782087a0e5c
Zhabskyi <poltavagaz@gmail.com>
2019-12-02 09:46:54 -0700

    Add error empty name validation in Form component and two tests for it

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from "../Button/Button";
 const Form = props => {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -16,6 +17,15 @@ const Form = props => {
     reset();
     props.onCancel();
   };
+
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }
 
   return (
     <main className='appointment__card appointment__card--create'>
@@ -30,6 +40,7 @@ const Form = props => {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -42,7 +53,7 @@ const Form = props => {
           <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={e => props.onSave(name, interviewer)} confirm>
+          <Button onClick={e => validate()} confirm>
             Save
           </Button>
         </section>
