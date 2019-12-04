278b68bec3df6797f7b004c997ab528d88a6d85e
Hafiz Suara <hafbau@yahoo.com>
2019-12-04 06:33:43 -0700

    added .env to ignore, removed proxy from package.json and put a console.log in axios instance for debugging

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 let instance;
 if (process.env.NODE_ENV !== "test") {
+  console.log("NOT TEST", process.env.API);
   instance = axios.create({
     baseURL: process.env.API
   });
